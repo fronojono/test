aa4aba84046d3f7d1b996e995a741f3c26604fce
meher frioui <meherfrioui2017@gmail.com>
2020-06-06 18:19:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+
+import { NavLink, Route, Switch, withRouter } from "react-router-dom";
+
 import './App.css';
+import Users from './component/users';
+import Posts from './component/posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+
+class App extends Component {
+  render() {
+    const { location: { pathname } } = this.props
+    console.log('props', this.props)
+    return (
+      <div >
+        <div className="top">
+          <ul className='nav__bar'>
+            <NavLink exact to='/user' activeClassName='active__class'><li>List Users</li></NavLink>
+            <NavLink exact to='/post' activeClassName='active__class'><li>List Posts</li></NavLink>
+            <NavLink exact to='new-post' activeClassName='active__class'><li>Add New Post</li></NavLink>
+            {pathname === '/post' && <input type="text" name="" id="" className='search' placeholder='search ...' />}
+
+
+          </ul>
+        </div>
+        <div className='App'>
+
+          <Switch>
+            <Route exact path='/user' render={() => <Users />} />
+            <Route exact path='/post' render={() => <Posts />} />
+          </Switch>
+
+        </div>
+
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withRouter(App);
